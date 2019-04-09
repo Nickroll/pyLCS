bab1e2f679e9692cd027d65a534de5b8823a8d66
Roller <rick.nickroller@gmail.com>
2019-04-09 07:42:23 -0400

    Begining crawler writing

--- pyLCS/crawler.py
+++ pyLCS/crawler.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+import requests_html
+
+
+def _create_connection(url: str, render: bool=False):
+    """_create_connection
+
+    Establishes a requests connection to the given page and returns the requests object 
+    :param url (str): The url to connect too.
+    :param render (bool): If the JS should be rendered or not.
+    """
+
