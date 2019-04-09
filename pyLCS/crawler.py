2d3fdfbc81c184c34bcb946ed8af02427e52616f
Roller <rick.nickroller@gmail.com>
2019-04-09 09:57:48 -0400

    Updating Environment file

--- pyLCS/crawler.py
+++ pyLCS/crawler.py
@@ -6,7 +6,8 @@ import requests_html
 def _create_connection(url: str, render: bool=False):
     """_create_connection
 
-    Establishes a requests connection to the given page and returns the requests object 
+    Establishes a requests connection to the given page and returns the requests object
+
     :param url (str): The url to connect too.
     :param render (bool): If the JS should be rendered or not.
     """
