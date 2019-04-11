60bc842c6c89626568a16dba9b8dbcdfee8040e9
Roller <rick.nickroller@gmail.com>
2019-04-11 10:19:52 -0400

    Removing old test file

--- tests/crawlTesting.py
+++
@@ -1,6 +0,0 @@
-from ..crawler import _create_connection
-
-conn_test = _create_connection('https://liquidpedia.net/leagueoflegends/LCS/2019/Spring/Group_Stage',
-                               render=True)
-links = conn_test.html.find('a[href*=matchhistory]')
-print(links)
