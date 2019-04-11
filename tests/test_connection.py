d7cee42316f600b4254d92112c61dbc3ef8d2224
Roller <rick.nickroller@gmail.com>
2019-04-11 10:18:52 -0400

    First tests passing

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 import context
+import responses
 from pyLCS import pyCrawler
+
+
+@responses.activate
+def test_create_connection_returns():
+    responses.add(responses.GET, 'http://test.com', status=200)
+    resp = pyCrawler._create_connection('http://test.com')
+
+    assert resp is not None
+
+
+@responses.activate
+def test_create_connection_bad_return():
+    responses.add(responses.GET, 'http://testfail.com', status=404)
+    resp = pyCrawler._create_connection('http://testfail.com')
+
+    assert resp is None
