15e96b97ae33add0c3619350285700f86ea38792
Roller <rick.nickroller@gmail.com>
2019-04-17 10:02:52 -0400

    Adding pytest fixture and working post game link retrieval

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -6,44 +6,48 @@ from pyLCS.exceptions import pyLCSExceptions
 from pyLCS.pyCrawler import pyCrawler
 
 
+@pytest.fixture
+def make_pyCrawler_base():
+    return pyCrawler(region='na', year='2019', split='spring', playoffs=False)
+
+
 @responses.activate
-def test_create_connection_returns():
+def test_create_connection_returns(make_pyCrawler_base):
     responses.add(responses.GET, 'http://test.com', status=200)
-    resp = pyCrawler._create_connection(url='http://test.com')
+    resp = make_pyCrawler_base._create_connection(url='http://test.com')
 
     assert resp is not None
 
 
 @responses.activate
-def test_create_connection_bad_return():
+def test_create_connection_bad_return(make_pyCrawler_base):
     responses.add(responses.GET, 'http://testfail.com', status=404)
-    resp = pyCrawler._create_connection(url='http://testfail.com')
+    resp = make_pyCrawler_base._create_connection(url='http://testfail.com')
 
     assert resp is None
 
 
-def test_ext_link_creation_return_type_tuple():
-    pyc = pyCrawler(region='na', year='2019', split='spring', playoffs=True)
-    extension = pyc._ext_link_creation()
+def test_ext_link_creation_return_type_tuple(make_pyCrawler_base):
+    make_pyCrawler_base.playoffs = True
+    extension = make_pyCrawler_base._ext_link_creation()
 
     assert isinstance(extension, tuple)
 
 
-def test_ext_link_creation_return_type_str():
-    pyc = pyCrawler(region='na', year='2019', split='spring', playoffs=False)
-    extension = pyc._ext_link_creation()
+def test_ext_link_creation_return_type_str(make_pyCrawler_base):
+    extension = make_pyCrawler_base._ext_link_creation()
 
     assert isinstance(extension, str)
 
 
-def test_ext_link_creation_return_type_list():
-    pyc = pyCrawler(region='lpl', year='2019', split='spring', playoffs=False)
-    extension = pyc._ext_link_creation()
+def test_ext_link_creation_return_type_list(make_pyCrawler_base):
+    make_pyCrawler_base.region = 'lpl'
+    extension = make_pyCrawler_base._ext_link_creation()
 
     assert isinstance(extension, list)
 
 
-def test_ext_link_creation_string():
+def test_ext_link_creation_string(make_pyCrawler_base):
     return_links = []
     check_list = ['https://liquipedia.net/leagueoflegends/LCS/2019/Spring/Group_Stage',
                   'https://liquipedia.net/leagueoflegends/LCS/Academy_League/2019/Spring/Group_Stage',
@@ -51,33 +55,33 @@ def test_ext_link_creation_string():
                   'https://liquipedia.net/leagueoflegends/LMS/2019/Spring/Group_Stage']
 
     for i in ['na', 'lck', 'lms', 'eu', 'academy']:
-        pyc = pyCrawler(region=i, year='2019', split='spring', playoffs=False)
-        return_links.append(pyc._ext_link_creation())
+        make_pyCrawler_base.region = i
+        return_links.append(make_pyCrawler_base._ext_link_creation())
 
     assert return_links.sort() == check_list.sort()
 
 
-def test_ext_link_creation_raises_error():
-    with pytest.raises(pyLCSExceptioins.RegionError):
-        pyc = pyCrawler(region='fails', year='2019', split='spring', playoffs=False)
-        pyc._ext_link_creation()
+def test_ext_link_creation_raises_error(make_pyCrawler_base):
+    with pytest.raises(pyLCSExceptions.RegionError):
+        make_pyCrawler_base.region = 'fails'
+        make_pyCrawler_base._ext_link_creation()
 
 
-def test_ext_link_creation_lpl():
+def test_ext_link_creation_lpl(make_pyCrawler_base):
     check_list = ['https://liquipedia.net/leagueoflegends/LPL/2019/Spring/Group_Stage/Week_1-5',
                   'https://liquipedia.net/leagueoflegends/LPL/2019/Spring/Group_Stage/Week_6-10']
 
-    pyc = pyCrawler(region='lpl', year='2019', split='spring', playoffs=False)
-    return_links = pyc._ext_link_creation()
+    make_pyCrawler_base.region = 'lpl'
+    return_links = make_pyCrawler_base._ext_link_creation()
 
     assert check_list.sort() == return_links.sort()
 
 
-def test_ext_link_creation_playoffs():
+def test_ext_link_creation_playoffs(make_pyCrawler_base):
     check_list = ['https://liquipedia.net/leagueoflegends/LCS/2019/Spring/Playoffs']
 
-    pyc = pyCrawler(region='lcs', year='2019', split='spring', playoffs=True)
-    ext, p_ext = pyc._ext_link_creation()
+    make_pyCrawler_base.playoffs = True
+    ext, p_ext = make_pyCrawler_base._ext_link_creation()
 
     assert check_list[0] == p_ext
 
@@ -86,12 +90,8 @@ HTML_BODY = '<html><body><a href="https://matchhistory.euw.leagueoflegends.com/e
 
 
 @responses.activate
-def test_retrieve_post_returns_list():
-    responses.add(responses.GET, 'http://test.com', stats=200, body=HTML_BODY)
-
-    pyc = pyCrawler(region='lcs', year='2019', split='spring', playoffs=False)
-    ext = pyc._ext_link_creation()
-
-    links = pyc._retrieve_post_match_site_links(ext, True)
+def test_retrieve_post_returns_list(make_pyCrawler_base):
+    responses.add(responses.GET, 'http://test.com', status=200, body=HTML_BODY)
+    links = make_pyCrawler_base._retrieve_post_match_site_links('http://test.com', False)
 
     assert isinstance(links, list)
