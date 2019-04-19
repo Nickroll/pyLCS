023e5714a8918b58638aace9c86733a0b6bc6251
Roller <rick.nickroller@gmail.com>
2019-04-19 11:20:50 -0400

    Building outline for json function

--- pyLCS/matchCrawler.py
+++ pyLCS/matchCrawler.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python
 
+from typing import Union
+
+from requests_html import HTMLSession
+
+
+class postMatchCrawl:
+    """postMatchCrawl
+
+    Crawls the post match links generated from liquidCrawler and gets the JSON data returned
+    """
+
+    def __init__(self, match_links: Union[list, str]=None):
+
+        if isinstance(match_links, str):
+            match_links = [match_links]
+
+        self.match_links = match_links
