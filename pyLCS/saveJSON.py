08f12f7d00fd2ad5f31414a1c65fc0f2ee31928c
Roller <rick.nickroller@gmail.com>
2019-05-06 12:27:51 -0400

    Adding the flatten function form stack overflow

--- pyLCS/saveJSON.py
+++ pyLCS/saveJSON.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python
+from typing import Union
+
+
+def flatten_json(y: dict=None) -> dict:
+    """flatten_json
+
+    Flattens a JSON to a single dict, take from:
+        https://stackoverflow.com/questions/51359783/python-flatten-multilevel-json
+
+    :param y (dict): The JSON dict to be flattened
+    :rtype dict
+    """
+    out = dict()
+
+    def flatten(x: Union(dict, list)=None, name: str=''):
+        """flatten
+
+        The actually flattening fucntion
+
+        :param x (Union(dict, list)): A dict or list object to be flattened
+        :param name (str): The name to append to the flattened object
+        """
+        if isinstance(x, dict):
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif isinstance(x, list):
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
