9560ee6fdf01479518ef51c328f2deb51ca214fd
Roller <rick.nickroller@gmail.com>
2019-05-08 15:00:29 -0400

    Adding example file for use when pre-testing

---
+++ example.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+
+
+from pyLCS import liquidCrawler, matchCrawler, saveJSON
+
+lc = liquidCrawler.liquidCrawler(region='na', year=2019, split='spring', playoffs=False)
+mls = lc.match_links(render=True)
+
+mc = matchCrawler.postMatchCrawl(mls[:2])
+json_data = mc.download_json_data()
+print(json_data['Timeline'])
