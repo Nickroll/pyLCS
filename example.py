90ef31250590d27992503ecbd7f063246f54f107
Roller <rick.nickroller@gmail.com>
2019-05-16 10:09:22 -0400

    Updating example with changes to matchCrawler

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ from pyLCS import liquidCrawler, matchCrawler, saveJSON
 lc = liquidCrawler.liquidCrawler(region='na', year=2019, split='spring', playoffs=False)
 mls = lc.match_links(render=True)
 
-mc = matchCrawler.postMatchCrawl(mls[:2])
-json_data = mc.download_json_data()
+mc = matchCrawler.download_json_data(mls[0])
 
-print(saveJSON.flatten_json(json_data))
+print(saveJSON.flatten_json(mc))
