70194c69c80ccc717ce0231d374627465a7a4be2
Roller <rick.nickroller@gmail.com>
2019-05-16 10:25:21 -0400

    Adding notes

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 # /usr/bin/env python
 
+import json
 
 from pyLCS import liquidCrawler, matchCrawler, saveJSON
 
@@ -7,5 +8,13 @@ lc = liquidCrawler.liquidCrawler(region='na', year=2019, split='spring', playoff
 mls = lc.match_links(render=True)
 
 mc = matchCrawler.download_json_data(mls[0])
+
+# NOTES: SQLite DBS: Player end match stats, player timeline stats, team match, team timeline
+# NOTES: gameId and player name is key for sqlite server
+# NOTES: Link timeline and match history
+# NOTES: Link player and team
+
+with open('test.json', 'w') as f:
+    json.dump(mc, f, indent=4)
 
 print(saveJSON.flatten_json(mc))
