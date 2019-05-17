139060b2920768717f0b3333d33a49b9074c6a36
Roller <rick.nickroller@gmail.com>
2019-05-17 08:38:02 -0400

    Starting the sqlite db creation, using pandas df

--- pyLCS/saveJSON.py
+++ pyLCS/saveJSON.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-
+import sqlite3
 from typing import Union
+
+import pandas
 
 
 def flatten_json(y: dict=None) -> dict:
@@ -36,3 +38,40 @@ def flatten_json(y: dict=None) -> dict:
 
     flatten(y)
     return out
+
+
+def _make_database(path: str=None) -> Union[sqlite3.Connection, None]:
+    """_make_database
+
+    Creates the sqlite3 database and returns the connection to it
+
+    :param path (str): The path to the database to create
+    :rtype Union(sqite3.Connection, None)
+    """
+
+    try:
+        conn = sqlite3.connect(path)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+def _create_table(conn: sqlite3.Connection=None, table_name: str=None) -> None:
+    """_create_table
+
+    Creates a table at the given sqlite connection passed to it
+
+    :param conn (sqlite3.Connection): A database to create a table at
+    :param table_name (str): The name of the table to create at the database
+    :rtype None
+    """
+
+    SQL = f'CREATE TABLE IF NOT EXISTS {table_name}'
+
+    try:
+        c = conn.cursor()
+        c.execute(SQL)
+    except sqlite3.Error as e:
+        print(e)
