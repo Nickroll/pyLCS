33a2b0cbb8edbcb349141529235eaadc08086257
Roller <rick.nickroller@gmail.com>
2019-05-30 10:49:13 -0400

    Refractoring match history parsing to new file

--- example.py
+++ example.py
@@ -2,13 +2,15 @@
 
 import json
 
-from pyLCS import liquidCrawler, matchCrawler, saveJSON
+from pyLCS import liquidCrawler, matchCrawler, parseMatchHist, saveJSON
 
 with open('test.json', 'r') as jf:
     data = json.load(jf)
 
-col_list = saveJSON.get_columns(data)
-stats = saveJSON.get_stats(data)
 
-saveJSON.make_sql_table('test.db', 'testtable', col_list)
+
+stats = parseMatchHist.get_stats(data)
+cols = parseMatchHist.get_columns(data)
+
+saveJSON.make_sql_table('test.db', 'testtable', cols)
 saveJSON.insert_stats('test.db', 'testtable', stats)
---
+++ pyLCS/parseMatchHist.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+import json
+from typing import Union
+
+
+def _flatten_json(y: dict=None) -> dict:
+    """flatten_json
+
+    Flattens a JSON to a single dict, take from:
+        https://stackoverflow.com/questions/51359783/python-flatten-multilevel-json
+
+    :param y (dict): The JSON dict to be flattened
+    :rtype dict
+    """
+    out = dict()
+
+    def flatten(x: Union[dict, list]=None, name: str=''):
+        """flatten
+
+        The actually flattening fucntion
+
+        :param x (Union(dict, list)): A dict or list object to be flattened
+        :param name (str): The name to append to the flattened object
+        """
+        if isinstance(x, dict):
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif isinstance(x, list):
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+
+def _parse_player_json_data(json_data: dict=None) -> dict:
+    """_parse_player_json_data
+
+    Flattens the JSON file then pulls out all the players data and retuns it as a dict
+    Example:
+        {gameId: {TL Impact: [stats], TL Doublelift: [stats]}}
+
+    :param json_data (dict): JSON data returned from the match history page
+    :rtype dict
+    """
+    cols = list()
+    flat_json = _flatten_json(json_data)
+    ret_dict = {flat_json['gameId']: []}
+
+    for i in range(0, 10):
+        key = f'participantIdentities_{i}_player_summonerName'
+        player_name = flat_json[key]
+        stats_key = f'ants_{i}_'
+        stats = [player_name]
+        stats.append(i)
+
+        for k, v in flat_json.items():
+            if stats_key in k.lower():
+                if 'antid' not in k.lower()[-5:]:
+                    if 'role' in k.lower() or 'lane' in k.lower():
+                        continue
+                    else:
+                        stats.append(v)
+                        cols.append(k)
+
+        ret_dict[flat_json['gameId']].append(stats)
+
+    return cols, ret_dict
+
+
+def _column_names_match_hist(col_data: list=None) -> list:
+    """_column_names_match_hist
+
+    Gets the names of the columns for the match history data
+
+    :param json_data (dict): JSON data returned from the match history page
+    :rtype list
+    """
+
+    stats_key = f'ants_0_'
+    ret_list = ['gameId', 'participantId']
+
+    for i in col_data:
+        if stats_key in i:
+            names = i.split('_')
+
+            if 'Deltas' in names[-2]:
+                ret_list.append(f'{names[-2]}_{names[-1]}')
+            else:
+                ret_list.append(names[-1])
+
+    ret_list.append('PlayerName')
+    return ret_list
+
+
+def _create_column_name_and_type(column_name: list=None, stats_data: dict=None) -> list:
+    """_create_column_name_and_type
+
+    Creates a list of [(column_name, column_type)] for all the columns to be used with SQL
+
+    :param column_name (list): The list of columns to be used in the SQL DB returned by _column_names_match_hist
+    :param stats_data (dict): The dict returned by _parse_player_json_data
+    :rtype list
+    """
+
+    ret_list = list()
+
+    # This is a terrible hack that needs to be fixed at some point
+    column_name.remove('PlayerName')
+    column_name.remove('gameId')
+
+    stats = list(stats_data.values())[0][0][1:]
+    tup_list = list(zip(column_name, stats))
+
+    for tup in tup_list:
+        name = tup[0]
+        if isinstance(tup[1], int):
+            col_type = 'real'
+        else:
+            col_type = 'text'
+
+        ret_list.append((name, col_type))
+
+    ret_list.extend([('gameId', 'real'), ('PlayerName', 'text')])
+
+    return ret_list
+
+
+def get_stats(json_data: Union[str, dict]=None) -> dict:
+    """get_stats
+
+    Takes either a JSON file, as str, or JSON data loaded via the JSON module. Will return the stats as:
+        {gameId: [[Player 1 Stats], [Player 2 Stats]], gameId2" [[]]}
+
+    :param json_file (Union[str, dict]): The path to the JSON file containing the stats or the JSON dict
+    :rtype dict
+    """
+    if isinstance(json_data, str):
+        with open(json_data, 'r') as jf:
+            data = json.load(jf)
+
+    elif isinstance(json_data, dict):
+        data = json_data
+
+    else:
+        raise TypeError(f'{type(json_data)} is wrong type, must be of type str or dict')
+
+    _, stats = _parse_player_json_data(data)
+
+    return stats
+
+
+def get_columns(json_data: Union[str, dict]=None) -> dict:
+    """get_columns
+
+    Takes a JSON file or JSON data leaded via the JSON module and retuns the column names for use in the
+    SQL table. Only needs to be run when creating the table the first time, or to check on the columns
+    being created
+
+    :param json_data (Union[str, dict]): The path to the JSON file or the JSON dict
+    :rtype dict
+    """
+
+    if isinstance(json_data, str):
+        with open(json_data, 'r') as jf:
+            data = json.load(jf)
+
+    elif isinstance(json_data, dict):
+        data = json_data
+
+    else:
+        raise TypeError(f'{type(json_data)} is wrong type, must be of type str or dict')
+
+    cols, stats = _parse_player_json_data(data)
+    cols = _column_names_match_hist(cols)
+    cols = _create_column_name_and_type(cols, stats)
+
+    return cols
