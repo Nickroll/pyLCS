b71c99f892797c861f0433bb719cf28c875e723b
Roller <rick.nickroller@gmail.com>
2019-07-17 13:33:46 -0400

    Updating example with new function

--- example.py
+++ example.py
@@ -8,18 +8,15 @@ from pyLCS import (liquidCrawler, matchCrawler, parseMatchHist, parseTimeline,
 with open('tlTest.json', 'r') as jf:
     tldata = json.load(jf)
 
-parse_data = parseTimeline._parse_tl_player_data(tldata)
 
 with open('test.json', 'r') as f:
     data = json.load(f)
 
 stats = parseMatchHist.get_stats(data)
-events = parseTimeline._parse_event_data(tldata, parse_data)
-fixed = parseTimeline._fix_pid_with_names(events, stats)
+fixed = parseTimeline.timeline_stats(tldata, stats, 15)
 
 with open('fixed.json', 'w') as jf:
     json.dump(fixed, jf, indent=4)
-
 
 # cols = parseMatchHist.get_columns(data)
 # saveJSON.make_sql_table('test.db', 'testtable', cols)
