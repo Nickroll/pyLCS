0a359ec6e28f5dcf7cfd92fc28b573e4691ff9a8
Roller <rick.nickroller@gmail.com>
2019-08-16 14:13:12 -0400

    Adding ext testing

--- pyLCS/strategies/liquid_strats.py
+++ pyLCS/strategies/liquid_strats.py
@@ -10,3 +10,15 @@ def build_liquidCrawler():
 
 def man_build_liquidCrawler():
     return st.tuples(st.text(), st.integers(min_value=0), st.text(), st.booleans())
+
+
+def build_validLiquidCrawler():
+    possible_regions = ['na', 'lcs', 'lck', 'korea', 'lms', 'eu', 'europe', 'lec', 'academy']
+    return st.builds(liquidCrawler, st.sampled_from(possible_regions), st.integers(min_value=0),
+                     st.text(), st.booleans())
+
+
+def build_invalid_regions():
+    possible_regions = ['na', 'lcs', 'lck', 'korea', 'lms', 'eu', 'europe', 'lec', 'academy']
+
+    return st.text().filter(lambda x: x not in possible_regions)
--- tests/test_liquidCrawler.py
+++ tests/test_liquidCrawler.py
@@ -5,8 +5,16 @@ import context
 import pytest
 import responses
 from hypothesis import given
+from pyLCS.exceptions import pyLCSExceptions
 from pyLCS.liquidCrawler import liquidCrawler
 from pyLCS.strategies import liquid_strats
+
+
+@pytest.fixture
+def create_liquidCrawler_base():
+    lc = liquidCrawler('na', 2019, 'spring', False)
+
+    return lc
 
 
 @given(liquid_strats.build_liquidCrawler())
@@ -19,13 +27,6 @@ def test_man_liquidCrawler_builds(t):
     lc = liquidCrawler(t[0], t[1], t[2], t[3])
 
     assert isinstance(lc.year, str)
-
-
-@pytest.fixture
-def create_liquidCrawler_base():
-    lc = liquidCrawler('na', 2019, 'spring', False)
-
-    return lc
 
 
 @responses.activate
@@ -42,3 +43,24 @@ def test_create_connection_invalid(create_liquidCrawler_base):
     resp = create_liquidCrawler_base._create_connection('https://invalidlink.com', render=False)
 
     assert resp is None
+
+
+@given(liquid_strats.build_validLiquidCrawler())
+def test_ext_creation_valid(lc):
+    resp = lc._ext_link_creation()
+
+    if lc.playoffs:
+        assert isinstance(resp, list)
+        assert 'Playoffs' in resp[1]
+    else:
+        assert isinstance(resp, str)
+        assert lc.year in resp
+        assert lc.split.capitalize() in resp
+
+
+@given(liquid_strats.build_invalid_regions())
+def test_ext_creation_invalid_region(create_liquidCrawler_base, r):
+    create_liquidCrawler_base.region = r
+
+    with pytest.raises(pyLCSExceptions.RegionError):
+        create_liquidCrawler_base._ext_link_creation()
