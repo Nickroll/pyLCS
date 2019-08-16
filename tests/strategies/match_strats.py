69881a6e3eeefc0df37a7bd2fc76979fbd5d1aa8
Roller <rick.nickroller@gmail.com>
2019-08-16 10:24:25 -0400

    Starting liquidCrawler testing

--- tests/strategies/match_strats.py
+++ tests/strategies/match_strats.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import hypothesis.strategies as st
 
 
--- tests/test_liquidCrawler.py
+++ tests/test_liquidCrawler.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-"""
 
-"""
 import context
 import pytest
 from hypothesis import given
 from hypothesis import stratagies as st
+from stratagies import liquid_strats
