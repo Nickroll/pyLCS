31d683da0f84c62623f663cdc633b6cd1df5495d
Roller <rick.nickroller@gmail.com>
2019-08-20 15:03:11 -0400

    comment clean up

--- example.py
+++ example.py
@@ -8,16 +8,11 @@ lc = liquidCrawler.liquidCrawler('na', 2019, 'Spring', False)
 links = lc.match_links()
 links = links[:3]
 
-
 json_data = matchCrawler.download_json_data(links)
 
-
-# with open('full_json.json', 'r') as jf:
-#     full_json = json.load(jf)
-#
 unwanted = {'SKILL_LEVEL_UP', 'ITEM_DESTROYED', 'ITEM_SOLD', 'WARD_PLACED', 'WARD_KILL',
             'ITEM_UNDO', 'ITEM_PURCHASED'}
-#
+
 merge = parseMatchHist.parse_match_history(json_data, 100, unwanted)
 
 with open('merge.json', 'w') as jf:
