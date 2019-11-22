fa46393c0ec3a1108a8fc7263dc229c75d634b93
Roller <rick.nickroller@gmail.com>
2019-11-22 08:20:12 -0500

    Removing extra info

--- example.py
+++ example.py
@@ -2,9 +2,10 @@
 
 import json
 
-from pyLCS.pyLCS import LCS
+from pyLCS.pyLCS import LCS, mongo_insert
+from pymongo import MongoClient
 
-lcs_data = LCS(region='all', year=2019, split='spring', playoffs=True)
+lcs_data = LCS(region='na', year=2019, split='spring', playoffs=True)
 lcs_data.match_history()
 
 unwanted = {'SKILL_LEVEL_UP', 'ITEM_DESTROYED', 'ITEM_SOLD', 'WARD_PLACED', 'WARD_KILL',
@@ -12,5 +13,9 @@ unwanted = {'SKILL_LEVEL_UP', 'ITEM_DESTROYED', 'ITEM_SOLD', 'WARD_PLACED', 'WAR
 
 output = lcs_data.parse_match_history(minute=15, unwanted_types=unwanted)
 
-with open('json_dump.json', 'w') as jf:
-    json.dump(output, jf, indent=4)
+
+client = MongoClient("")
+db = client['test']
+
+for i in ['player', 'team', 'gameinfo']:
+    mongo_insert(output, i, db, i, check='gameId')
