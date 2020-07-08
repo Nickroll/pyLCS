b21c9d3fea93df37102ce9c07c658eb7d7fe4ce9
Roller <rick.nickroller@gmail.com>
2020-07-08 13:43:36 -0400

    Now uses config.ini

--- pyLCS/matchHistory.py
+++ pyLCS/matchHistory.py
@@ -6,152 +6,99 @@ history pages and returns them for use later.
 """
 
 from typing import Union
-
+import configparser
 from .connection import create_connection
 from .exceptions import pyLCSExceptions
 
 
-class matchHistory(object):
-    """matchHistory
+# TODO: Make this read from a config file of some sort
+def _ext_link_creation() -> list:
+    """_post_match_game_links
 
-    Basic class for creating and retriving the post match history links from liquidpedia
+    Uses liquidpedia to retrieve the links to the post match game links for the given region
+
+    :rtype Union[list]
     """
 
-    def __init__(self, region: str=None, year: Union[str, int]=None, split: str=None, playoffs: bool=False):
-        self.region = region
+    link_list = list()
 
-        if str(year).isnumeric():
-            self.year = str(year)
-        else:
-            raise(TypeError('Year must be of an int or a numeric string'))
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    parse_dict = {sec: dict(config.items(sec)) for sec in config.sections()}
 
-        self.split = split
+    base = parse_dict['Links']['base']
+    group = parse_dict['Links']['group']
+    year = parse_dict['Links']['year']
+    if parse_dict['Links']['playoffs'].lower() == 'true':
+        playoffs = True
 
-        if isinstance(playoffs, bool):
-            self.playoffs = playoffs
-        else:
-            raise(TypeError('Playoffs must be of type bool'))
+    for k, v in parse_dict['Regions'].items():
+        if v.lower() == 'true':
+            reg = k.upper()
+            sp = parse_dict['Splits'][k].split(' ')
 
-    # TODO: Make this read from a config file of some sort
-    def _ext_link_creation(self) -> Union[str, list]:
-        """_post_match_game_links
+            if len(sp) == 2:
+                tmp = [f'{base}{reg}/{year}/{sp[0]}/{group}', f'{base}{reg}/{year}/{sp[1]}/{group}']
+                link_list.extend(tmp)
+            else:
+                tmp = [f'{base}{reg}/{year}/{sp}/{group}']
+                link_list.extend(tmp)
 
-        Uses liquidpedia to retrieve the links to the post match game links for the given region
+            if playoffs:
+                link_list.extend([f'{i}/Playoffs' for i in tmp])
 
-        :rtype Union[str, list]
-        """
+    return link_list
 
-        base_link = 'https://liquipedia.net/leagueoflegends/'
 
-        # Make a dict for clearner code
-        if self.region.lower() in ['na', 'lcs']:
-            ext = [f'{base_link}LCS/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() in ['lck', 'korea']:
-            ext = [f'{base_link}LCK/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'lms':
-            ext = [f'{base_link}LMS/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() in ['eu', 'europe', 'lec']:
-            ext = [f'{base_link}LEC/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'academy':
-            ext = [f'{base_link}LCS/Academy_League/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'lcl':
-            ext = [f'{base_link}LCL/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'ljl':
-            ext = [f'{base_link}LJL/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'lla':
-            ext = [f'{base_link}LLA/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'vcs':
-            ext = [f'{base_link}VCS/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'opl':
-            ext = [f'{base_link}OPL/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'tcl':
-            ext = [f'{base_link}TCL/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'cblol':
-            ext = [f'{base_link}CBLOL/{self.year}/{self.split.capitalize()}/Group_Stage']
-        elif self.region.lower() == 'brcc':
-            ext = [f'{base_link}BRCC/{self.year}/{self.split.capitalize()}/Group_Stage']
+def _retrieve_post_match_site_links(ext_link: list, render: bool) -> list:
+    """_retrieve_post_match_site_links
 
-        elif self.region.lower() == 'all':
-            ext = []
-            spring_sum_regions = ['LCS', 'LCK', 'LEC', 'LJL', 'LCL', 'VCS', 'LMS']
-            for r in spring_sum_regions:
-                tmp_list = [f'{base_link}{r}/{self.year}/Spring/Group_Stage',
-                            f'{base_link}{r}/{self.year}/Summer/Group_Stage']
-                ext.extend(tmp_list)
+    Uses xpath to find the href links to the match history pages
 
-            wint_sum_regions = ['TCL', 'CBLOL', 'BRCC']
-            for r in wint_sum_regions:
-                tmp_list = [f'{base_link}{r}/{self.year}/Winter/Group_Stage',
-                            f'{base_link}{r}/{self.year}/Summer/Group_Stage']
-                ext.extend(tmp_list)
+    :param ext_link (str): The links returned by _ext_link_creation
+    :param render (bool): If the page should be rendered for JS
+    :rtype list
+    """
 
-            ext.extend([f'{base_link}LLA/{self.year}/Closing/Group_Stage', f'{base_link}LLA/{self.year}/Opening/Group_Stage',
-                        f'{base_link}OPL/{self.year}/Split_1/Group_Stage', f'{base_link}OPL/{self.year}/Split_2/Group_Stage',
-                        f'{base_link}LCS/Academy_League/{self.year}/Summer/Group_Stage', f'{base_link}LCS/Academy_League/{self.year}/Spring/Group_Stage'])
+    ret_links = []
 
-        else:
-            raise pyLCSExceptions.RegionError(f'{self.region} is not one of LCS, LCK, LMS, '
-                                              'LEC, LCL, LJL, LLA, VCS, OPL, academy, '
-                                              'TCL, or all')
+    for el in ext_link:
+        r = create_connection(link=el, render=render)
 
-        if self.playoffs:
-            p_ext = list()
-            for e in ext:
-                p_ext.append(f'{e[:-11]}Playoffs')
+        if not r:
+            continue
 
-            ext.extend(p_ext)
-            return ext
+        if not r.text:
+            continue
 
-        else:
-            return ext
+        links = r.html.xpath('//a[starts-with(@title, "Match History")]/@href')
 
-    def _retrieve_post_match_site_links(self, ext_link: list, render: bool) -> list:
-        """_retrieve_post_match_site_links
-
-        Uses xpath to find the href links to the match history pages
-
-        :param ext_link (str): The links returned by _ext_link_creation
-        :param render (bool): If the page should be rendered for JS
-        :rtype list
-        """
-
-        ret_links = []
-
-        for el in ext_link:
-            r = create_connection(link=el, render=render)
-
-            if not r:
+        # Check for empty links and remove them
+        for l in links:
+            if l in ['', ' ']:
                 continue
+            else:
+                ret_links.append(l)
 
-            if not r.text:
-                continue
+        if len(ret_links) == 0:
+            raise(pyLCSExceptions.LinkLenError('Length of links retrieved was 0. '
+                                               'Either extensions were not properly created or '
+                                               'All hrefs were empty (xpath may be broken)'))
 
-            links = r.html.xpath('//a[starts-with(@title, "Match History")]/@href')
+    return ret_links
 
-            # Check for empty links and remove them
-            for l in links:
-                if l in ['', ' ']:
-                    continue
-                else:
-                    ret_links.append(l)
 
-            if len(ret_links) == 0:
-                raise(pyLCSExceptions.LinkLenError('Length of links retrieved was 0. '
-                                                   'Either extensions were not properly created or '
-                                                   'All hrefs were empty (xpath may be broken)'))
+def match_links(render: bool=True) -> list:
+    """match_links
 
-        return ret_links
+    Retrieves the links to the match history pages from the liquidpedia website.
 
-    def match_links(self, render: bool=True) -> list:
-        """match_links
+    :param render (bool): If JavaScript should be rendered on the page
+    :rtype list
+    """
 
-        Retrieves the links to the match history pages from the liquidpedia website.
+    exts = _ext_link_creation()
+    links = _retrieve_post_match_site_links(ext_link=exts, render=render)
 
-        :param render (bool): If JavaScript should be rendered on the page
-        :rtype list
-        """
+    return links
 
-        exts = self._ext_link_creation()
-        links = self._retrieve_post_match_site_links(ext_link=exts, render=render)
-
-        return links
